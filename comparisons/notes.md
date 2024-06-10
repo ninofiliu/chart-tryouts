6677267a34d1a331a4589f85e97bdd763dc9aeb1
Nino Filiu <nino.filiu@gmail.com>
2024-06-10 16:53:43 +0200

    try out chart.js

---
+++ comparisons/notes.md
@@ -0,0 +1,5 @@
+# Chart.js
+
+Can dynamically update the config, but on serie data change, redraws the whole serie
+
+Errors are hard to understand. API can feel awkward. I just couldn't make a linechart with time x axis. The code from the official demo didn't work.
--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,66 @@
-export const App = () => <></>;
+import Chart from "chart.js/auto";
+import { useState } from "react";
+
+const data = Array(20)
+  .fill(null)
+  .map((_, index) => ({
+    index,
+    float: Math.random(),
+    category: `cat-${index}`,
+    date: new Date(2000 + index, 1, 1),
+  }));
+
+export const App = () => {
+  const [width, setWidth] = useState(500);
+  const [height, setHeight] = useState(500);
+  const [chart, setChart] = useState<Chart | null>(null);
+
+  return (
+    <div
+      className="container"
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+      }}
+    >
+      <canvas
+        ref={(elt) => {
+          if (!elt) return;
+          if (chart) return;
+          setChart(
+            new Chart(elt, {
+              type: "line",
+              data: {
+                labels: data.map((row) => row.category),
+                datasets: [
+                  {
+                    label: "Acquisitions by year",
+                    data: data.map((row) => row.float),
+                    borderColor: "red",
+                    backgroundColor: "pink",
+                  },
+                ],
+              },
+              options: {
+                maintainAspectRatio: false,
+              },
+            })
+          );
+        }}
+      />
+      <button
+        className="resizer"
+        onMouseDown={(evtDown) => {
+          const onMove = (evtMove: MouseEvent) => {
+            setWidth(width + evtMove.clientX - evtDown.clientX);
+            setHeight(height + evtMove.clientY - evtDown.clientY);
+          };
+          document.addEventListener("mousemove", onMove);
+          document.addEventListener("mouseup", () => {
+            document.removeEventListener("mousemove", onMove);
+          });
+        }}
+      ></button>
+    </div>
+  );
+};
